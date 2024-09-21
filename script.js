e7a9963dbb5b7d064de076bde4b21fbbef3b6219
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-09-21 19:36:41 +0800

    Feat: create newScoreBoard function that monitors game dynamicsie score, whose turn

--- script.js
+++ script.js
@@ -17,8 +17,32 @@ function newPlayer(name, symbol) {
     const giveWin = () => wins++;
     const getName = () => name;
     const getSymbol = () => symbol;
-    return {getWins, giveWin, getName, getSymbol};
+    const resetWins = () => wins = 0;
+    return {getWins, giveWin, getName, getSymbol, resetWins};
+}
+
+function newScoreBoard(player1, player2) {
+    let turn = player1;
+    function displayScore() {
+        console.log(`${player1.getName()}: ${player1.getWins()} | ${player2.getName()}: ${player2.getWins()}`);
+    }
+    function resetScore() {
+        player1.resetWins();
+        player2.resetWins();
+    }
+    function changeTurn() {
+        if (turn === player1) {
+            turn = player2;
+        } else {
+            turn = player1;
+        }
+    }
+    function getTurn() {
+        return turn;
+    }
+    return {displayScore, resetScore, changeTurn, getTurn};
 }
 
 const chris = newPlayer("Chris", "X");
 const chelsea = newPlayer("Chelsea", "O");
+const scoreBoardCC = newScoreBoard(chris, chelsea);
